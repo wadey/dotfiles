f95586748d30aebb1f3575d65ad40b8fbb8342fb
Wade Simmons <wade@wades.im>
2012-04-02 13:20:44 -0700

    git_prompt_help

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -6,13 +6,27 @@ if [ "$USER" = "wade" -o "$USER" = "Wade" ]; then
     export GIT_COMMITTER_EMAIL="wade@wades.im"
 fi
 
-GIT_PS1_SHOWDIRTYSTATE=1
-GIT_PS1_SHOWSTASHSTATE=1
-GIT_PS1_SHOWUNTRACKEDFILES=1
-GIT_PS1_SHOWUPSTREAM=auto
+GIT_PS1_SHOWDIRTYSTATE=${GIT_PS1_SHOWDIRTYSTATE:-1}
+GIT_PS1_SHOWSTASHSTATE=${GIT_PS1_SHOWSTASHSTATE:-1}
+GIT_PS1_SHOWUNTRACKEDFILES=${GIT_PS1_SHOWUNTRACKEDFILES:-1}
+GIT_PS1_SHOWUPSTREAM=${GIT_PS1_SHOWUPSTREAM:-auto}
 
 __git_prompt() {
   __git_ps1 "[%s]" | sed 's/%/%%/g'
 }
+
+git_prompt_help() {
+    echo "*     Unstaged files"
+    echo "+     Staged files"
+    echo "%     Untracked files"
+    echo "$     Stashed files exist"
+    echo "#     No HEAD (new repo)"
+    echo
+    echo "=     equal to upstream"
+    echo ">     Ahead of upstream"
+    echo "<     Behind upstream"
+    echo "<>    Diverged from upstream"
+}
+
 
 source $ZSH_CUSTOM/git-completion.bash
