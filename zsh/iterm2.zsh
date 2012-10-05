d8be7564a0c5351697112b333fdd3415bed638fe
Wade Simmons <wade@wades.im>
2012-10-05 15:32:08 -0700

    iterm2 tab colors

--- zsh/iterm2.zsh
+++ zsh/iterm2.zsh
@@ -1,31 +1,30 @@
 # iTerm2 tab color commands
 # http://code.google.com/p/iterm2/wiki/ProprietaryEscapeCodes
-tab-color() {
-    echo -ne "\033]6;1;bg;red;brightness;$1\a"
-    echo -ne "\033]6;1;bg;green;brightness;$2\a"
-    echo -ne "\033]6;1;bg;blue;brightness;$3\a"
-}
-tab-red() { tab-color 255 0 0 }
-tab-green() { tab-color 0 255 0 }
-tab-blue() { tab-color 0 0 255 }
-tab-reset() { echo -ne "\033]6;1;bg;*;default\a" }
 
-tab-exec() {
-    trap "tab-reset" EXIT INT
-    $1
-    shift
-    $*
-}
-color-ssh() {
-    if [[ -n "$ITERM_SESSION_ID" ]]; then
-        trap "tab-reset" INT EXIT
-        if [[ "$*" =~ "production|ec2-.*compute-1" ]]; then
-            tab-color 255 0 0
+if [[ -n "$ITERM_SESSION_ID" ]]; then
+    tab-color() {
+        echo -ne "\033]6;1;bg;red;brightness;$1\a"
+        echo -ne "\033]6;1;bg;green;brightness;$2\a"
+        echo -ne "\033]6;1;bg;blue;brightness;$3\a"
+    }
+    tab-red() { tab-color 255 0 0 }
+    tab-green() { tab-color 0 255 0 }
+    tab-blue() { tab-color 0 0 255 }
+    tab-reset() { echo -ne "\033]6;1;bg;*;default\a" }
+
+    function iterm2_precmd() {
+        tab-reset
+    }
+
+    function iterm2_preexec() {
+        if [[ "$1" =~ "^ssh " ]]; then
+            tab-color 160 255 160
         else
-            tab-color 0 255 0
+            tab-color 160 160 255
         fi
-    fi
-    ssh $*
-}
-compdef _ssh color-ssh=ssh
-alias ssh=color-ssh
+    }
+
+    autoload -U add-zsh-hook
+    add-zsh-hook precmd  iterm2_precmd
+    add-zsh-hook preexec iterm2_preexec
+fi
