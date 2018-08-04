f0313bdf9611760070351f35b638cbd030935b9c
Wade Simmons <wade@wades.im>
2018-08-04 16:18:33 -0400

    add script to generate autokey-gtk mappings
    This is my quest to make the macOS shortcuts burned into my memory work
    in Gnome.

---
+++ autokey/gen.py
@@ -0,0 +1,82 @@
+import json
+import re
+
+import yaml
+
+def replchar(matchobj):
+    return hex(ord(matchobj.group(0)))
+
+def keystr_to_name(keystr):
+    s = re.sub(r'[<>]', '', keystr)
+    s = re.sub(r'[+]', '-', s)
+    s = re.sub(r'[^-A-Za-z0-9]', replchar, s)
+    return s
+
+def default_json():
+    return {
+        "usageCount": 0,
+        "omitTrigger": False,
+        "prompt": False,
+        # "description": "",
+        "abbreviation": {
+            "wordChars": "[\\w]",
+            "abbreviations": [],
+            "immediate": False,
+            "ignoreCase": False,
+            "backspace": True,
+            "triggerInside": False
+        },
+        # "hotkey": {
+        #     "hotKey": "",
+        #     "modifiers": [
+        #         "<super>"
+        #     ]
+        # },
+        "modes": [
+            3
+        ],
+        "showInTrayMenu": False,
+        "filter": {
+            "regex": None,
+            "isRecursive": False
+        },
+        "type": "script",
+        "store": {}
+    }
+
+if __name__ == '__main__':
+    config_data = yaml.load(open('keys.yml'))
+
+    for k, v in config_data.items():
+        modifiers = k.split('+')
+        hotkey = modifiers.pop()
+        name = keystr_to_name(k)
+        j = default_json()
+        j["description"] = name
+        j["hotkey"] = {
+                "hotKey": hotkey,
+                "modifiers": modifiers,
+        }
+        with open('gen/.%s.json' % name, 'w') as f:
+            f.write(json.dumps(j, indent=4))
+            f.write("\n")
+
+        with open('gen/%s.py' % name, 'w') as f:
+            if isinstance(v, basestring):
+                v = {"default": v}
+            default = v["default"]
+            del v["default"]
+            if len(v) == 0:
+                f.write("keyboard.send_keys('%s')\n" % default)
+            else:
+                first = True
+                for app, key in v.items():
+                    if first:
+                        f.write("if ")
+                        first = False
+                    else:
+                        f.write("elif ")
+                    f.write("window.get_active_class() == '%s':\n" % app)
+                    f.write("    keyboard.send_keys('%s')\n" % key)
+                f.write("else:\n")
+                f.write("    keyboard.send_keys('%s')\n" % default)
